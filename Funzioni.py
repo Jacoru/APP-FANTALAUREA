8ccc8f8ffa09352c9dbf73107d2f2a2817791475
Jacopo <jacopo.024@gmail.com>
2026-03-20 16:38:48 +0100

    Update Funzioni.py

--- Funzioni.py
+++ Funzioni.py
@@ -5,15 +5,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-s3 = boto3.client(
-    "s3",
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    region_name=os.getenv("AWS_REGION")
-)
-
+def get_s3_client():
+    return boto3.client(
+        "s3",
+        aws_access_key_id=st.secrets["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=st.secrets["AWS_SECRET_ACCESS_KEY"],
+        region_name=st.secrets["AWS_REGION"]
+    )
 
 def download_to_s3():
+    s3 = get_s3_client()
+
     bucket_name = "fanta-laurea"
     object_key = "persone.csv"
 
